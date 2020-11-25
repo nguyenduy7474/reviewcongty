92bc342a37b39557a129f62733ef233515997641
nguyenduy7474 <nguyenduy7474@gmail.com>
2020-11-25 10:54:10 +0700

    change database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://127.0.0.1/reviewcongty' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-//'url' : 'mongodb://nguyenduy7475:duy123654789@ds263078.mlab.com:63078/singwithyou'    
-//'url' : 'mongodb://nguyenduy7475:duy123654789@ds021046.mlab.com:21046/9done'
+    //'url' : 'mongodb://127.0.0.1/reviewcongty'
+    "url": "mongodb://nguyenduy7475:duy123654789@ds139251.mlab.com:39251/reviewcongty"
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
 };
